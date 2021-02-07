41810add1569d276c7a13adc5ce1d750212633c1
Pablo <pawelrokicki@yahoo.com>
2021-02-07 01:02:03 +0100

    clean and add tinydb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 from functools import partial
+from tinydb import TinyDB
 
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -21,61 +22,62 @@ import kivy
 Config.set('graphics', 'width', '200')
 Config.set('graphics', 'height', '650')
 
-# kivy.require('1.9.0')
-
 class RootLayout(BoxLayout):
     pass
 
 class TrackerContainer(BoxLayout):
     ID = NumericProperty(0)
-    has_timer = NumericProperty(0)
+    timer_on = NumericProperty(0)
     name = StringProperty('')
     timer_event = None
     total_duration = NumericProperty(0)
     current_duration = NumericProperty(0)
     
-class PlayButton(Button):
-    btn_source = StringProperty('play-button.png')
-
 class TimeTracker(App):
     def build(self):
         self.trackers_count = -1
-        #TODO: dictionary of ids and corresponding indices
+        self.trackers_indices = {}
+        self.db = TinyDB('db.json')
+
         Builder.load_file('root.kv')
         self.root = RootLayout()
         return self.root
 
-    def find_tracker_index(self, timer_id):
-        for i in range(len(self.root.ids['box'].children)):
-            if self.root.ids['box'].children[i].ID == timer_id:
-                index = i
-        return index
+    def match_ids_to_indices(self):
+        for index in range(len(self.root.ids['box'].children)):
+            id = self.root.ids['box'].children[index].ID
+            self.trackers_indices[id] = index
 
     def start_timer(self, timer_id):
-        timer_index = self.find_tracker_index(timer_id)
-        
         now = datetime.datetime.now()
-        event = Clock.schedule_interval(partial(self.update_label, start_time=now, timer_id=timer_index), 0.1)
+        event = Clock.schedule_interval(partial(self.update_label, start_time=now, timer_id=timer_id), 0.1)
         return event
 
     def stop_timer(self, timer, timer_id):
-        timer_index = self.find_tracker_index(timer_id)
+        timer_index = self.trackers_indices.get(timer_id)
         
         self.root.ids['box'].children[timer_index].total_duration += self.root.ids['box'].children[timer_index].current_duration
         timer.cancel()
 
     def update_label(self, dt, start_time, timer_id):
+        timer_index = self.trackers_indices.get(timer_id)
+
         diff = datetime.datetime.now() - start_time
-        self.root.ids['box'].children[timer_id].current_duration = diff.total_seconds()
+        self.root.ids['box'].children[timer_index].current_duration = diff.total_seconds()
         
-        total = self.root.ids['box'].children[timer_id].current_duration + self.root.ids['box'].children[timer_id].total_duration
-        self.root.ids['box'].children[timer_id].ids['time'].text = str(datetime.timedelta(seconds=total)).split('.')[0]
+        total = self.root.ids['box'].children[timer_index].current_duration + self.root.ids['box'].children[timer_index].total_duration
+        self.root.ids['box'].children[timer_index].ids['time'].text = str(datetime.timedelta(seconds=total)).split('.')[0]
 
     def create_new_tracker(self, new_name):
         self.trackers_count += 1
         new_id = self.trackers_count
+
         self.root.ids['box'].add_widget(TrackerContainer(ID=new_id, name=new_name))
         self.root.ids['box'].height += TrackerContainer.height.defaultvalue
+
+        self.match_ids_to_indices()
+
+        self.db.insert({'tracker_name': new_name, 'tracker_id': new_id, 'total_time': 0, 'past_data': {} })
 
 if __name__ == '__main__':
     TimeTracker().run()
